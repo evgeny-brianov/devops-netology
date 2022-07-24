48e4ed4ddbe36f10dee71b0f3e88769e39b82b5e
evgeny-brianov <95703090+evgeny-brianov@users.noreply.github.com>
2022-07-24 14:16:12 +0700

    Update main.tf

--- 7.3/src/bucket/main.tf
+++ 7.3/src/bucket/main.tf
@@ -1,1 +1,25 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
 
+variable "yc_token" { type= string }
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = "b1gecqsa22e178i9vkau"
+  folder_id = "b1gbcsll4bftts9kdd4b"
+  zone      = "ru-central1-a"
+}
+
+variable "key_id" { type= string }
+variable "sec" { type= string }
+resource "yandex_storage_bucket" "bucket-netology" {
+  access_key = var.key_id
+  secret_key = var.sec
+  bucket = "bucket-netology"
+force_destroy = true
+}
